670393adc6ffc10e2acf14f906479c92e84a173b
tfh0007 <tfh0007@auburn.edu>
2021-11-12 19:14:57 -0600

    Upgraded the main menu

--- ThomasUI/MainWindow.py
+++ ThomasUI/MainWindow.py
@@ -2,27 +2,30 @@ import os
 import PySimpleGUI as sg
 import subprocess
 
+from humanfriendly import text
+
 # Simple UI for catan
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Game using default settings')],
-            [sg.Text('Please specify the type of player for players 1,2,3, and 4')],
-            [sg.Text('Options:')],
-            [sg.Text('1. AlphaBeta - Oracle player AI')],
-            [sg.Text('2. Human - Human Player ')],
-            [sg.Text('3. BaseLine - A Base Line AI that picks moves randomly ')],
-            [sg.Text('4. Custom1 - The first AI we made')],
-            [sg.Text('5. Custom2 - The second custom AI we made')],
-            [sg.Text('6. None - This player will not exist. At least 2 players must exist')],
-            [sg.Text('Player 1:          '), sg.InputText()],
-            [sg.Text('Player 2:          '), sg.InputText()],
-            [sg.Text('Player 3:          '), sg.InputText("None")],
-            [sg.Text('Player 4:          '), sg.InputText("None")],
-            [sg.Text('Give Number of Games as 1,2,3,4,...n')],
-            [sg.Text('Num of Games:'), sg.InputText("1")],
+layout = [  [sg.Text('Define Game Rules',font=("Arial",25))],[sg.Radio('Default     ', "RADIO1", default=True, size=(10,1)), sg.Radio('Custom', "RADIO1")],
+            [sg.Text('Please specify the type of player for players 1,2,3, and 4',font=("Arial",15))],
+            [sg.Text('Options:',font=("Arial",15))],
+            [sg.Text('1. AlphaBeta - Oracle player AI',font=("Arial",15))],
+            [sg.Text('2. Human - Human Player ',font=("Arial",15))],
+            [sg.Text('3. BaseLine - A Base Line AI that picks moves randomly ',font=("Arial",15))],
+            [sg.Text('4. Custom1 - The first AI we made',font=("Arial",15))],
+            [sg.Text('5. Custom2 - The second custom AI we made',font=("Arial",15))],
+            [sg.Text('6. None - This player will not exist. At least 2 players must exist',font=("Arial",15))],
             
-            [sg.Button('Start Game'), sg.Button('Cancel')] ]
+            [sg.Text('Player 1:     ',font=("Arial",15)), sg.InputCombo(('AlphaBeta', 'Human', 'BaseLine','Custom1','Custom2'), default_value = 'AlphaBeta',size=(20, 5))],
+            [sg.Text('Player 2:     ',font=("Arial",15)), sg.InputCombo(('AlphaBeta', 'Human', 'BaseLine','Custom1','Custom2'), default_value = 'BaseLine',size=(20, 5))],
+            [sg.Text('Player 3:     ',font=("Arial",15)), sg.InputCombo(('None','AlphaBeta', 'Human', 'BaseLine','Custom1','Custom2'),default_value = 'None', size=(20, 5))],
+            [sg.Text('Player 4:     ',font=("Arial",15)), sg.InputCombo(('None','AlphaBeta', 'Human', 'BaseLine','Custom1','Custom2'),default_value = 'None', size=(20, 5))],
+            [sg.Text('Give Number of Games as 1,2,3,4,...100',font=("Arial",15))],
+            [sg.Text('Num of Games:',font=("Arial",15)), sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=1)],
+            [sg.Text('',font=("Arial",15))],
+            [sg.Button('Start Game',font=("Arial",15)), sg.Button('Cancel',font=("Arial",15))] ]
 
 # Create the Window
 window = sg.Window('Settlers of Catan', layout)
@@ -53,13 +56,13 @@ while True:
                 
                 
                 
-        print('You entered:', '\nPlayer1:', values[0], '\nPlayer2:', values[1], '\nPlayer3:', values[2], '\nPlayer4:', values[3])
+        print('You entered:', '\nPlayer1:', values[2], '\nPlayer2:', values[3], '\nPlayer3:', values[4], '\nPlayer4:', values[5])
     
 
         
         #rc = subprocess.call("catanatron-play --players=R,W,F,AB:2 --num=2")
         # This is the path on a Mac for Thomas
-        os.system('/Users/thomashansknecht/opt/anaconda3/bin/catanatron-play --players=' + str(values[0]) +','+ str(values[1])+ ','+ str(values[2])+ ',' + str(values[3]) + ' --num=' + str(values[4])) 
+        os.system('/Users/thomashansknecht/opt/anaconda3/bin/catanatron-play --players=' + str(values[2]) +','+ str(values[3])+ ','+ str(values[4])+ ',' + str(values[5]) + ' --num=' + str((int(values[6])))) 
         break
 
 window.close()
